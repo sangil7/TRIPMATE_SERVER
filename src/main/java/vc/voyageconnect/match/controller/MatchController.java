58c1ea1ce55a7faac2eb16e4fece62959f81a513
dlwns <dlwns0904@naver.com>
2024-10-04 21:12:07 +0900

    1

--- src/main/java/vc/voyageconnect/match/controller/MatchController.java
+++ src/main/java/vc/voyageconnect/match/controller/MatchController.java
@@ -5,19 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import vc.voyageconnect.match.domain.MatchProfile;
 
 @Controller
 @Slf4j
 public class MatchController {
 
-    @PostMapping("/match/addProfile")
+    /*@PostMapping("/match/addProfile")
     public void addProfile(MatchProfile matchProfile) {
 
-        /**
+        *//**
          * POST로 받아서 매칭 프로필 저장하는 로직
-         */
-    }
+         *//*
+    }*/
 
     @GetMapping("/match/profile/{userId}")
     public void viewProfile() {
